6159d8ff6c613930b7fe65ab18880e0fdfba83f7
Angelina Li <ali6@wellesley.edu>
2017-08-14 21:48:13 +0800

    replaced spreadsheet access w/ urls

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import config.user_info as usr
 from config.gsheets import client
 
 # 2 sheets of data required
-contacts = client.open(usr.contacts_sht).sheet1
-reimbs = client.open(usr.rmbs_sht).sheet1
+contacts = client.open_by_url(usr.contacts_url).sheet1
+reimbs = client.open_by_url(usr.rmbs_url).sheet1
