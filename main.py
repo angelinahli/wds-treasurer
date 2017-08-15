3e99bc5d9b2fa968b69ef9210b96c73f9412c98d
Angelina Li <ali6@wellesley.edu>
2017-08-15 16:08:30 +0800

    added fns to get new reimbs data

--- main.py
+++ main.py
@@ -12,31 +12,31 @@ class Template:
     be filled with data.
     """
 
-    def _save_file(self, wb, filename):
+    def _save_file(self, wb, filepath):
         """
-        Saves file if filename is specified.
+        Saves file if filepath is specified.
         """
-        if filename:
-            wb.save(filename)
+        if filepath:
+            wb.save(filepath)
 
-    def get_empty(self, filename=False):
+    def get_empty(self, filepath=None):
         """
         Returns empty template.
-        Optionally saves as an Excel workbook if filename is
+        Optionally saves as an Excel workbook if filepath is
         specified.
         """
-        wb = load_workbook(filename='template.xlsx')
-        self._save_file(wb, filename)
+        wb = load_workbook(filepath='template.xlsx')
+        self._save_file(wb, filepath)
         return wb
 
-    def get_new(self, filename=False):
+    def get_new(self, filepath=None):
         """
         Returns new template with org_name, bookkeeper,
         treasurer and address stub variables filled.
-        Optionally saves as an Excel workbook if filename is
+        Optionally saves as an Excel workbook if filepath is
         specified.
         """
-        wb = load_workbook(filename = 'template.xlsx')
+        wb = load_workbook(filepath = 'template.xlsx')
         ws = wb.active
 
         ws[usr.tmp_vars['org_name']] = usr.org_name
@@ -44,14 +44,14 @@ class Template:
         ws[usr.tmp_vars['treasurer']] = usr.treasurer
         ws[usr.tmp_vars['address']] = usr.address
 
-        self._save_file(wb, filename)
+        self._save_file(wb, filepath)
         return wb
 
-    def get_completed(self, data_dict, filename=False):
+    def get_completed(self, data_dict, filepath=None):
         """
         Return completed form given a dictionary of values
         to plug into the template.
-        Optionally saves as an Excel workbook if filename is
+        Optionally saves as an Excel workbook if filepath is
         specified.
 
         data_dict: Dictionary of user specific data for each form, where
@@ -59,27 +59,26 @@ class Template:
         """
         wb = self.get_new()
         ws = wb.active
-
         for varname in data_dict:
             # only available data will be filled in
             ws[usr.tmp_vars[varname]] = data_dict[varname]
-
-        self._save_file(wb, filename)
+        self._save_file(wb, filepath)
         return wb
 
 # (2) get data for each user
 
-def get_stu_dict():
+def get_user_dict():
     """
     returns dictionary of dictionaries for each user (student member),
     with username as key, and user's dictionary data as values.
     """
-    cols = usr.contact_cols
+    cols = usr.contacts_cols
     contacts = client.open_by_url(usr.contacts_url).sheet1
     user_dict = {}
-
-    for row in range(2, len(row_count) + 1):
+    for row in range(2, contacts.row_count):
         data = contacts.row_values(row)
+        if not any(data):
+            break
         user_dict[ data[cols['username'] - 1] ] = {
             'stu_name': data[cols['stu_name'] - 1],
             'stu_id': data[cols['stu_id'] - 1],
@@ -88,6 +87,67 @@ def get_stu_dict():
 
     return user_dict
 
-# 2 sheets of data required
-# reimbs = client.open_by_url(usr.rmbs_url).sheet1
+def get_new_row_range(ws):
+    """
+    given a gspread worksheet representing the form accepting new
+    reimbursements, returns a list of the row range that have not
+    yet been processed.
+    """
+    row_range = []
+    for row in range(2, ws.row_count):
+        # if timestamp is missing data assumed to be missing
+        if not ws.cell(row, usr.rmbs_cols['date']):
+            break
+        # has form is None or false if no form has been created yet
+        if not ws.cell(row, usr.rmbs_cols['has_form']):
+            row_range.append(row)
+    return row_range
 
+def update_rows(ws, row_range):
+    """
+    given a worksheet and a list of row range, updates rows to indicate a 
+    form has been created for each of those rows.
+    """
+    for row in row_range:
+        ws.update_cell(row, usr.rmbs_cols['has_form'], True)
+    print "rows {} have been processed!".format(row_range)
+
+def get_rmbs_dict(row_data):
+    """
+    converts a list of reimbursements data for a given row to a dictionary
+    of that data.
+    """
+    new_data = {}
+    for var in usr.rmbs_cols:
+        new_data[var] = row_data[rmbs_cols[var] - 1]
+    return new_data
+
+def get_data_dict(ws, row_number, user_data):
+    """
+    given a gspread worksheet of the form accepting new reimbursements,
+    the row number to access and a dictionary of data for each user,
+    returns a dictionary of all data associated with that row of reimbursements.
+    """
+    rmbs_data = get_rmbs_dict(ws.row_values(row))
+    username = rmbs_data[usr.rmbs_cols['username']]
+    try:
+        user_values = dict(user_data[username], **rmbs_data)
+    except KeyError:
+        raise ValueError("User {} has not submitted their info to the contacts form".format(username))
+    return {var: user_values.get(var, None) for var in usr.tmp_vars}
+
+def get_worksheet_data(ws, row_range):
+    """
+    given a gspread worksheet representing the form accepting new 
+    reimbursements and list of row range, returns a list of dictionaries 
+    that can be used to create spreadsheets.
+    """
+    user_data = get_user_dict()
+    return [get_data_dict(ws, row, user_data) for row in row_range]
+
+# (3) make spreadsheets for users and update converted rows
+
+def make_spreadsheets():
+    """
+    """
+    rmbs = client.open_by_url(usr.rmbs_url).sheet1
