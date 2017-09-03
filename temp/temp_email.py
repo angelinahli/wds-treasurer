8aa0bc2be5feabd137be94e04b3b093523f17368
Angelina Li <ali6@wellesley.edu>
2017-09-02 22:26:37 -0400

    formatted output messages

--- temp/temp_email.py
+++ temp/temp_email.py
@@ -4,4 +4,4 @@ temp_body = """
 Once forms have been signed, printed and delivered, please remember to check them off at: {}
 
 The following files are attached:
-""".format("test_url")
+""".format(usr.RMBS_URL)
