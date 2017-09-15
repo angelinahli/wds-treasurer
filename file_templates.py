dabd95c08568f728151712aa9ffedc8a416e56f7
Angelina Li <ali6@wellesley.edu>
2017-09-15 10:18:40 -0400

    made worksheet objects

--- file_templates.py
+++ file_templates.py
@@ -1,11 +1,12 @@
 from openpyxl import load_workbook
 from openpyxl.drawing.image import Image
 from openpyxl.styles.borders import Border, Side
+from re import split
 
 import config.user_info as usr
 from config.gsheets import client
 
-class FormTemplate:
+class Form:
     """
     Represents an Excel form template, that can additionally
     be filled with data.
@@ -74,21 +75,75 @@ class FormTemplate:
         self._save_file(wb)
         return wb
 
-    def get_completed(self, data_dict):
+    def get_completed(self, form_data):
         """
         Return completed form given a dictionary of values
         to plug into the template.
         Optionally saves as an Excel workbook if filepath is
         specified.
 
-        data_dict: Dictionary of user specific data for each form, where
+        form_data: Dictionary of user specific data for each form, where
         keys are strings corresponding to usr.tmp_vars key names. 
         """
         wb = self.get_new()
         ws = wb.active
-        for varname in data_dict:
+        for varname in form_data:
             # only available data will be filled in
-            ws[usr.tmp_vars[varname]] = data_dict[varname]
+            ws[usr.tmp_vars[varname]] = form_data[varname]
         self._calculate_fund_total(ws)
         self._save_file(wb)
         return wb
+
+class StudentsFile:
+    """
+    Represents an additional text file that lists the names of students
+    who were associated with this reimbursement.
+    """
+
+    def __init__(self, filepath=None):
+        self.filepath = filepath
+
+    def _split_student_usernames(self, usernames):
+        """
+        usernames (string): a string list of usernames.
+        converts this into a list of string usernames for further use.
+        """
+        return split('\s*,\s*', usernames)
+
+    def _get_students_data(self, usernames, user_data):
+        """
+        usernames (string): string list of usernames.
+        user_data (dictionary): dictionary of dictionaries mapping a students
+        username to their contact data.
+        returns a list of dictionaries of students and their contact info.
+        """
+        usernames = self._split_student_usernames(usernames)
+        try:
+            student_data = [user_data[user] for user in usernames]
+        except KeyError:
+            error_msg = """A user from below has not submitted contact info.
+            List of student usernames: {}""".format(", ".join(usernames))
+            raise ValueError(error_msg)
+        return student_data
+
+    def get_new_file(self, usernames, user_data):
+        """
+        usernames (string): string list of usernames.
+        user_data (dictionary): dictionary of dictionaries mapping a students
+        username to their contact data.
+        """
+        print "Generating file {}".format(self.filepath)
+        file_body = "This reimbursement paid for the following students " + \
+                    "(Banner ID included in brackets for each):"
+        student_data = self._get_students_data(usernames, user_data)
+        
+        with open(self.filepath, "w") as students_file:
+            students_file.write(file_body)
+            for student in student_data:
+                line = "\n- {name} ({id})".format(
+                    name=student['stu_name'],
+                    id=student['stu_id'])
+                students_file.write(line)
+
+
+
---
+++ worksheets.py
@@ -0,0 +1,75 @@
+import gspread
+from re import split
+
+import config.user_info as usr
+from config.gsheets import client
+
+class Worksheet:
+
+    def __init__(self, url, column_values, start_row, is_ordered=True):
+        """
+        url (string): the URL of a google sheet
+        column_values (dict): a dictionary of the worksheets column names and
+        their google sheets column indices
+        start_row (int): the google sheets row index where the first line of
+        data starts
+        is_ordered (boolean): indicates whether data in this worksheet is filled
+        from the starting row down
+        """
+        self.url = url
+        self.column_values = column_values
+        self.start_row = start_row
+        self.is_ordered = is_ordered
+        self.ws = client.open_by_url(self.url).sheet1
+
+
+class UserWorksheet(Worksheet):
+
+    def get_user_data():
+        """
+        returns dictionary of dictionaries for each user, with username as key
+        and user data dictionary as values.
+        """
+        data_column_values = {
+            var: value - 1 for var, value in self.column_values 
+            if key != "username"}
+        user_dict = {}
+        for row_num in range(self.start_row, self.ws.row_count + 1):
+            row_data = self.ws.row_values(row_num)
+            if self.is_ordered and not any(row_data):
+                # stop checking rest of rows
+                break
+            username = row_data[self.column_values["username"] - 1]
+            user_dict[username] = {
+                var: row_data[data_column_values[var]] for var in 
+                data_column_values}
+        return user_dict
+
+class ReimbursementWorksheet(Worksheet):
+
+    def get_new_data_range():
+        """
+        returns a list of the row numbers that haven't yet been processed.
+        """
+        row_range = []
+        for row_num in range(self.start_row, self.ws.row_count + 1):
+            has_date = self.ws.cell(row_num, self.column_values["date"]).value
+            if self.is_ordered and not has_date:
+                break
+            if not self.ws.cell(row_num, self.column_values["has_form"]).value:
+                row_range.append(row_num)
+        return row_range
+
+    def get_rmbs_row_data(row_data):
+        """
+        row_data (list): list of data from a single row.
+        returns all reimbursement data associated with specified row.
+        """
+        return {var: row_data[self.column_values[var] - 1] for var in
+            self.column_values}
+
+    def update_processed_rows(row_range):
+        for row_num in row_range:
+            self.ws.update_cell(row_num, self.column_values["has_form"], True)
+        print "Row range: {} has been processed!".format(row_range)
+
